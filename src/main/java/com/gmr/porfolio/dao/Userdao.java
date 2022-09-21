d263079a59a91a03c9f9e183c0885fc2f2c4d282
Gabusy07 <100946627+Gabusy07@users.noreply.github.com>
2022-09-21 15:16:17 -0300

    dao

--- src/main/java/com/gmr/porfolio/dao/Userdao.java
+++ src/main/java/com/gmr/porfolio/dao/Userdao.java
@@ -1,4 +1,18 @@
 package com.gmr.porfolio.dao;
 
-public class Userdao {
+import com.gmr.porfolio.models.User;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+public interface Userdao {
+
+
+    void editUser(Long id);
+
+    void deleteUser(Long id);
+
+    void addUser(User u);
+
+    User getUserData(User u) throws NoSuchAlgorithmException, InvalidKeySpecException;
 }
--- src/main/java/com/gmr/porfolio/dao/UserdaoImpl.java
+++ src/main/java/com/gmr/porfolio/dao/UserdaoImpl.java
@@ -1,4 +1,54 @@
 package com.gmr.porfolio.dao;
 
-public class UserdaoImpl {
+import com.gmr.porfolio.models.Encrypt;
+import com.gmr.porfolio.models.User;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.List;
+
+@Repository
+@Transactional
+public class UserdaoImpl implements Userdao {
+
+    @PersistenceContext
+    EntityManager entManager;
+
+    @Override
+    public void editUser(Long id) {
+
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+
+    }
+
+    @Override
+    public void addUser(User u) {
+
+    }
+
+    @Override
+    public User getUserData(User u) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        String query = "FROM User WHERE id= :id";
+        final List list = entManager.createQuery(query).setParameter("id", u.getId())
+                .getResultList();
+
+        if (list.isEmpty()) {
+            return null;
+        }
+
+
+        User user = (User) list.get(0);
+        String hashedPass = user.getPassword();
+        if (Encrypt.validatePassword(u.getPassword(), hashedPass)) {
+            return user;
+        }
+        return null;
+    }
 }
