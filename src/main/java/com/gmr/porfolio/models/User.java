200afbf10f6a1d6f313152f93eff1a4b79117b0a
Gabusy07 <100946627+Gabusy07@users.noreply.github.com>
2022-09-21 13:44:25 -0300

    corrigiendo errores

--- src/main/java/com/gmr/porfolio/models/User.java
+++ src/main/java/com/gmr/porfolio/models/User.java
@@ -3,24 +3,26 @@ package com.gmr.porfolio.models;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
-
-
-
 public class User {
 
-    private int id;
-    @Getter @Setter
+    @Getter
+    @Setter
+    private Long id;
+
+    @Getter
+    @Setter
     private String name;
 
-    @Getter @Setter
+    @Getter
+    @Setter
     private String lastname;
 
-    @Getter @Setter
+    @Getter
+    @Setter
     private String password;
 
-    @Getter @Setter
+    @Getter
+    @Setter
     private String email;
 
 }
-
