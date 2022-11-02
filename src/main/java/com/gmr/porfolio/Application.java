af51b2112f77eee3bd73f9db33510737a8200275
Gabusy07 <100946627+Gabusy07@users.noreply.github.com>
2022-11-02 15:45:43 -0300

    cloud google

--- src/main/java/com/gmr/porfolio/Application.java
+++ src/main/java/com/gmr/porfolio/Application.java
@@ -3,10 +3,7 @@ package com.gmr.porfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+
 
 import java.util.Arrays;
 
