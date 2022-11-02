349c353ecf1096521e9eb93d7977b0bdc85b05a2
Gabusy07 <100946627+Gabusy07@users.noreply.github.com>
2022-11-01 22:45:26 -0300

    a

--- src/main/java/com/gmr/porfolio/Application.java
+++ src/main/java/com/gmr/porfolio/Application.java
@@ -18,5 +18,22 @@ public class Application {
 
 
 	}
-	
+	@Bean
+	public CorsFilter corsFilter() {
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true); // you USUALLY want this
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("HEAD");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("DELETE");
+		config.addAllowedMethod("PATCH");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
 }
